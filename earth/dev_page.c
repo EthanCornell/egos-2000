5b82aa2d49ca7c5d940392bdc246cd19e729e133
Ethan Huang <112031418+EthanCornell@users.noreply.github.com>
2023-11-16 03:02:29 -0500

    Update dev_page.c
    updated LRU cache

--- earth/dev_page.c
+++ earth/dev_page.c
@@ -19,19 +19,46 @@
 
 int cache_slots[ARTY_CACHED_NFRAMES]; // Array to keep track of cache slots, with size based on the defined number of cache frames.
 char *pages_start = (void*)FRAME_CACHE_START; // Pointer to the start of the frame cache memory area.
+int last_used[ARTY_CACHED_NFRAMES]; // Array to keep track of the last used time for each slot. last_used array stores the time when each cache slot was last used.
 
-static int cache_eviction() {        // Function to handle cache eviction.
-    int idx = rand() % ARTY_CACHED_NFRAMES;  // Selects a random cache slot to evict.
-    int frame_id = cache_slots[idx]; // Retrieves the frame ID of the selected cache slot.
 
+//Random Eviction Strategy: Random eviction can potentially evict pages that are frequently accessed, leading to more cache misses.
+// static int cache_eviction() {        // Function to handle cache eviction.
+//     int idx = rand() % ARTY_CACHED_NFRAMES;  // Selects a random cache slot to evict.
+//     int frame_id = cache_slots[idx]; // Retrieves the frame ID of the selected cache slot.
+
+//     // Writes the evicted frame back to disk.
+//     earth->disk_write(frame_id * NBLOCKS_PER_PAGE, NBLOCKS_PER_PAGE, pages_start + PAGE_SIZE * idx);
+//     return idx;                      // Returns the index of the evicted cache slot.
+// }
+
+// Least Recently Used (LRU) algorithm. In LRU, the cache slot that hasn't been used for the longest time is selected for eviction. 
+//The cache_eviction function finds the least recently used slot by iterating through last_used.
+static int cache_eviction() {
+    // Find the least recently used cache slot.
+    int lru_idx = 0;
+    for (int i = 1; i < ARTY_CACHED_NFRAMES; i++) {
+        if (last_used[i] < last_used[lru_idx]) {
+            lru_idx = i;
+        }
+    }
+
+    int frame_id = cache_slots[lru_idx];
     // Writes the evicted frame back to disk.
-    earth->disk_write(frame_id * NBLOCKS_PER_PAGE, NBLOCKS_PER_PAGE, pages_start + PAGE_SIZE * idx);
-    return idx;                      // Returns the index of the evicted cache slot.
+    earth->disk_write(frame_id * NBLOCKS_PER_PAGE, NBLOCKS_PER_PAGE, pages_start + PAGE_SIZE * lru_idx);
+    return lru_idx;
 }
 
-void paging_init() {                 // Function to initialize paging.
-    memset(cache_slots, 0xFF, sizeof(cache_slots)); // Initializes the cache_slots array, setting all values to -1 (0xFF).
+//The paging_init function initializes the last_used array.
+void paging_init() {
+    memset(cache_slots, 0xFF, sizeof(cache_slots)); // Initializes the cache_slots array.
+    memset(last_used, 0, sizeof(last_used)); // Initializes the last_used array.
 }
+
+//For Random Eviction Strategy
+// void paging_init() {                 // Function to initialize paging.
+//     memset(cache_slots, 0xFF, sizeof(cache_slots)); // Initializes the cache_slots array, setting all values to -1 (0xFF).
+// }
 
 int paging_invalidate_cache(int frame_id) { // Function to invalidate a specific frame in the cache.
     for (int j = 0; j < ARTY_CACHED_NFRAMES; j++)  // Iterates through all cache slots.
