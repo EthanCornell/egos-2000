343cdb92c816cdf28f2a0312537390079394b38b
Ethan Huang <112031418+EthanCornell@users.noreply.github.com>
2023-11-16 12:51:06 -0500

    Update ult.c
    add comments

--- apps/user/ult.c
+++ apps/user/ult.c
@@ -22,31 +22,34 @@ void ctx_switch(void** old_sp, void* new_sp);
 void thread_exit();
 
 /** Multi-threading functions **/
+//uses setjmp and longjmp for context switching. When setjmp is called, it saves the current environment (including the instruction pointer and stack pointer) in jmp_buf. 
+//Later, longjmp is used to restore this environment, effectively switching the context.
 #define MAX_THREADS 10
 
 // Thread structure
 struct thread {
-    void (*function)(void*);
-    void *arg;
-    bool is_active;
-    jmp_buf env; // Environment buffer to store the thread context
+    void (*function)(void*); // Pointer to the thread function
+    void *arg;               // Argument to be passed to the thread function
+    bool is_active;          // Flag to indicate whether the thread is active
+    jmp_buf env;             // Environment buffer to store the thread context for context switching
 };
 
-struct thread threads[MAX_THREADS];
-int current_thread_idx = -1;
+struct thread threads[MAX_THREADS]; // Array of threads
+int current_thread_idx = -1;        // Index of the currently running thread, -1 indicates no thread is currently active
 
 // Initialize thread management
 void thread_init() {
     for (int i = 0; i < MAX_THREADS; ++i) {
-        threads[i].is_active = false;
+        threads[i].is_active = false; // Initialize all threads as inactive
     }
 }
 
 // Entry point for threads
 void ctx_entry() {
     if (current_thread_idx >= 0 && threads[current_thread_idx].is_active) {
+        // If there is an active thread, execute its function
         threads[current_thread_idx].function(threads[current_thread_idx].arg);
-        thread_exit(); // Exit the thread after function execution
+        thread_exit(); // Exit the thread after executing its function
     }
 }
 
@@ -54,17 +57,18 @@ void ctx_entry() {
 void thread_create(void (*f)(void *), void *arg) {
     for (int i = 0; i < MAX_THREADS; ++i) {
         if (!threads[i].is_active) {
+            // Find an inactive thread slot and set up the thread
             threads[i].function = f;
             threads[i].arg = arg;
             threads[i].is_active = true;
 
             if (setjmp(threads[i].env) == 0) {
-                // Save the thread context and return
+                // Save the current context (like a bookmark) and return to continue execution
                 return;
             } else {
-                // When longjmp is called, start the thread function
+                // This part is executed when longjmp is called, starting the thread function
                 threads[i].function(threads[i].arg);
-                thread_exit();
+                thread_exit(); // Exit the thread after the function completes
             }
         }
     }
@@ -73,14 +77,16 @@ void thread_create(void (*f)(void *), void *arg) {
 // Yield execution to another thread
 void thread_yield() {
     int current_thread = current_thread_idx;
-    int next_thread = (current_thread + 1) % MAX_THREADS;
-    current_thread_idx = next_thread;
+    int next_thread = (current_thread + 1) % MAX_THREADS; // Determine the next thread to run
+    current_thread_idx = next_thread; // Update the index of the current thread
 
     if (threads[current_thread].is_active) {
         if (setjmp(threads[current_thread].env) == 0) {
+            // Save the current thread's context and switch to the next thread
             longjmp(threads[next_thread].env, 1);
         }
     } else {
+        // If the current thread is not active, just switch to the next thread
         longjmp(threads[next_thread].env, 1);
     }
 }
@@ -88,16 +94,19 @@ void thread_yield() {
 // Exit the current thread
 void thread_exit() {
     if (current_thread_idx >= 0) {
+        // Mark the current thread as inactive
         threads[current_thread_idx].is_active = false;
-        // Find the next active thread and switch to it
+        // Loop to find the next active thread to switch to
         for (int i = (current_thread_idx + 1) % MAX_THREADS; i != current_thread_idx; i = (i + 1) % MAX_THREADS) {
             if (threads[i].is_active) {
+                // Switch to the next active thread
                 current_thread_idx = i;
-                longjmp(threads[i].env, 1);
+                longjmp(threads[i].env, 1); // Jump to the next thread's saved context
             }
         }
     }
 }
+
 
 
 // /** Multi-threading functions **/
